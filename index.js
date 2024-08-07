918d5c3f7bba593561109fd66ce4ac796f1ed816
Shreeram <shreeram1162004@gmail.com>
2024-08-07 19:05:56 +0530

    user added

--- index.js
+++ index.js
@@ -26,47 +26,85 @@ const db = new pg.Client({
 });
 db.connect();
 
-
+let currentUser=1;
+let currentUserName='Null'
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const day=new Date().getDay();
 const month = new Date().getMonth();
-let tasks=[];
+let tasks=[]
 
 async function getList() {
-  const result= await db.query("SELECT * FROM tasks order by id desc");
+  const result= await db.query("SELECT * FROM tasks  WHERE user_id=$1 order by id desc",[currentUser]);
   tasks= result.rows;
+}
+async function getName() {
+  const result= await db.query("SELECT * FROM users where id=$1 ",[currentUser]);
+  currentUserName=result.rows[0].name;
 }
   app.get("/",async (req,res)=>{
    await getList();
+   await getName();
  const quotes=randomQuotes.default();
     res.render("index",{
       quote: quotes["body"] ,
       author:quotes["author"],
       task: tasks,
+      name:currentUserName,
         day:allDay[day],
         month : allMonth[month]
     });
   }); 
 app.post("/submit",async(req,res)=>{ 
- 
-
  const task =req.body["input"];
-await db.query("INSERT INTO tasks (title) VALUES ($1)",[task]);
+await db.query("INSERT INTO tasks (title,user_id) VALUES ($1,$2)",[task,currentUser]);
 res.redirect("/");
 });
 
 app.post("/delete",async(req,res)=>{
  
    const id=req.body.itemId;
-
-
 await db.query("DELETE from tasks where id=$1",[id]);
    res.redirect("/");
   
 });
+
+app.post("/addUser", async (req,res)=>{
+  try{
+  const userName=req.body.userName;
+  currentUserName=userName;
+  const id= await db.query("INSERT INTO users (name) VALUES ($1) returning id ",[userName]);
+  currentUser=id.rows[0].id;
+  res.redirect("/");}catch(error){
+    res.render("newUser.ejs",{error:"user already exist"});
+  }
+})
+app.post("/getUser", async (req,res)=>{
+try{
+  const userName=req.body.userName;
+  const id = await db.query("SELECT id  from users where name=$1",[userName]);
+  currentUser=id.rows[0].id;
+  res.redirect("/");}
+  catch(error){
+    const quotes=randomQuotes.default();
+    res.render("index",{
+      quote: quotes["body"] ,
+      author:quotes["author"],
+      task: tasks,
+      name:currentUserName,
+        day:allDay[day],
+        month : allMonth[month],
+        error:"user doesn't exist"
+    });
+  }
+})
+
+app.get("/newUser",(req,res)=>{
+  res.render("newUser.ejs");
+})
+
 app.listen(port, () => {
  
     console.log(`Listening on port ${port}`);
