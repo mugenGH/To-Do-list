3d134839e697fa2644b4e73775f5f6f3f6d881a5
Shreeram <shreeram1162004@gmail.com>
2024-08-07 09:58:26 +0530

    added db

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 import express from "express";
 import bodyParser from "body-parser";
 import randomQuotes from "random-quotes";
+import path from 'path';  // Import path module
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import pg from "pg";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = 3000;
+app.set('views', path.join(__dirname, 'views'));
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
 
-app.use(express.static("public"));
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+
+const port = 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "todolist",
+  password: "123456",
+  port: 5432,
+});
+db.connect();
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const day=new Date().getDay();
 const month = new Date().getMonth();
-const tasks=[];
+let tasks=[];
 
-  app.get("/",(req,res)=>{
-   
+async function getList() {
+  const result= await db.query("SELECT * FROM tasks order by id desc");
+  tasks= result.rows;
+}
+  app.get("/",async (req,res)=>{
+   await getList();
  const quotes=randomQuotes.default();
-    res.render("index.ejs",{
+    res.render("index",{
       quote: quotes["body"] ,
       author:quotes["author"],
       task: tasks,
@@ -23,18 +50,20 @@ const tasks=[];
         month : allMonth[month]
     });
   }); 
-app.post("/submit",(req,res)=>{ 
+app.post("/submit",async(req,res)=>{ 
  
 
-  tasks[tasks.length]=req.body["input"];
+ const task =req.body["input"];
+await db.query("INSERT INTO tasks (title) VALUES ($1)",[task]);
 res.redirect("/");
 });
 
-app.post("/delete",(req,res)=>{
+app.post("/delete",async(req,res)=>{
  
-   const index=tasks.indexOf(req.body.item);
-   tasks.splice(index,1);
+   const id=req.body.itemId;
 
+
+await db.query("DELETE from tasks where id=$1",[id]);
    res.redirect("/");
   
 });
